84ec24f4cd1a6a8d7294ef4e0433164a3f577a48
Austin Smith <AssailantLF@gmail.com>
2015-01-31 20:03:53 -0800

    added ch2 project 1

---
+++ Ch2/ProgrammingProjects/p1.c
@@ -0,0 +1,22 @@
+/* Write a program that uses printf to display
+ * the following picture on the screen: 
+ *        *
+ *       *
+ *      *
+ * *   *
+ *  * *
+ *   *
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("       *\n");
+	printf("      * \n");
+	printf("     *  \n");
+	printf("*   *   \n");
+	printf(" * *    \n");
+	printf("  *     \n");
+
+	return 0;
+}
