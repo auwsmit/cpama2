20f38043315fae6ac21dea1d38d3797791d2a6a2
Austin Smith <AssailantLF@gmail.com>
2015-01-31 20:56:33 -0800

    added ch2 project 8

---
+++ Ch2/ProgrammingProjects/p8.c
@@ -0,0 +1,30 @@
+/* Write a program that calculates the remaining balance on a loan after the
+ * first, second, and third monthly payments.
+ *
+ * Display each balance with two digits after the decimal point.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float loan, interest, monthlyPayment;
+
+	printf("Enter the amount of loan: ");
+	scanf("%f", &loan);
+	printf("Enter interest rate: ");
+	scanf("%f", &interest);
+	printf("Enter monthly payment: ");
+	scanf("%f", &monthlyPayment);
+
+	/* interest is yearly, so this converts it to a monthly rate */
+	interest = (interest * 0.01f) / 12;
+
+	loan = (loan - monthlyPayment) + (loan * interest);
+	printf("\nBalance remaining after first payment:  $%.2f", loan);
+	loan = (loan - monthlyPayment) + (loan * interest);
+	printf("\nBalance remaining after second payment: $%.2f", loan);
+	loan = (loan - monthlyPayment) + (loan * interest);
+	printf("\nBalance remaining after third payment:  $%.2f", loan);
+
+	return 0;
+}
