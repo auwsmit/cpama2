b26c772c83fcdf7791c1edbaa8f5fe4877285f88
Austin Smith <AssailantLF@gmail.com>
2015-01-31 20:35:01 -0800

    added ch2 project 5

---
+++ Ch2/ProgrammingProjects/p5.c
@@ -0,0 +1,23 @@
+/* Write a program that asks the user to enter a value for x and then displays
+ * the value of the following polynomial: (3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("The following polynomial will be calculated:\n");
+	printf("(3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6)\n\n");
+
+	printf("Enter a number x: ");
+	scanf("%d", &x);
+	printf("\n");
+
+	x = (3 * (x*x*x*x*x)) + (2 * (x*x*x*x))
+		- (5 * (x*x*x)) - (x*x) + (7 * x) - 6;
+
+	printf("Answer: %d", x);
+
+	return 0;
+}
