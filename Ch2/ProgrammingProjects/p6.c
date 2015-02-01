2b4268ba96a3eae5a108a84621787340a0a7d016
Austin Smith <AssailantLF@gmail.com>
2015-01-31 20:39:03 -0800

    added ch2 project 6

---
+++ Ch2/ProgrammingProjects/p6.c
@@ -0,0 +1,23 @@
+/* Modify the program of Programming Project 5 so that the polynomial is
+ * evaluated using the following formula:
+ * ((((3x + 2)x - 5)x - 1)x + 7)x - 6
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("The following polynomial will be calculated:\n");
+	printf("(3x + 2)x - 5)x - 1)x + 7)x - 6)\n\n");
+
+	printf("Enter a number x: ");
+	scanf("%d", &x);
+	printf("\n");
+
+	x = (((((((((3 * x) + 2) * x) - 5) * x) - 1) * x) + 7) * x) - 7;
+
+	printf("Answer is: %d", x);
+
+	return 0;
+}
