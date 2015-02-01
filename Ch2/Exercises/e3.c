4d0779972aff34b1b952b18e56371e4d5e71bb47
Austin Smith <AssailantLF@gmail.com>
2015-01-31 18:46:27 -0800

    added ch2 exercise 3

---
+++ Ch2/Exercises/e3.c
@@ -0,0 +1,18 @@
+/* Condence the dweight.c program by (1) replacing assignments to height,
+ * length, and width with initializers and (2) removing the weight variable,
+ * instead calculating (volume+165) / 166 with the last printf.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int height = 8, length = 12, width = 10, volume;
+
+	volume = height * length * width;
+
+	printf("Dimensions: %dx%dx%d\n", length, width, height);
+	printf("Volume (cubic inches): %d\n", volume);
+	printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166);
+
+	return 0;
+}
