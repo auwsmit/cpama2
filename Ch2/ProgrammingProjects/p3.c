4dda1f13d0a5bdfb249589671ff027f654ef0c0b
Austin Smith <AssailantLF@gmail.com>
2015-01-31 20:26:20 -0800

    added ch2 project 3

---
+++ Ch2/ProgrammingProjects/p3.c
@@ -0,0 +1,19 @@
+/* Modify the program of Programming Project 2 so that
+ * it prompts the user for the radius of the sphere.
+ */
+#include <stdio.h>
+
+#define NUM_PIE 3.141592653f
+
+int main(void)
+{
+	int radius;
+
+	printf("Enter the radius of a sphere: ");
+	scanf("%d", &radius);
+
+	printf("The volume of a %d-meter sphere is: %.2f\n",
+		radius, (4.0f/3.0f) * NUM_PIE * (radius * radius * radius));
+
+	return 0;
+}
