85de795ce0d10bea96751aa8dbe7422379415f2a
Austin Smith <AssailantLF@gmail.com>
2015-01-31 17:42:40 -0800

    added file dweight2.c

---
+++ Ch2/dweight2.c
@@ -0,0 +1,23 @@
+/* Computes the dimensional weight of a
+box from input provided by the user */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int height, length, width, volume, weight;
+
+	printf("Enter height of box: ");
+	scanf("%d", &height);
+	printf("Enter length of box: ");
+	scanf("%d", &length);
+	printf("Enter width of box: ");
+	scanf("%d", &width);
+	volume = height * length * width;
+	weight = (volume + 165) / 166;
+
+	printf("Volume (cubic inches): %d\n", volume);
+	printf("Dimensional weight (pounds): %d\n", weight);
+
+	return 0;
+}
