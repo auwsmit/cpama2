aaf4a383495189c8ce6b9ecc26581b388575baa0
Austin Smith <AssailantLF@gmail.com>
2015-01-31 20:44:33 -0800

    added ch2 project 7

---
+++ Ch2/ProgrammingProjects/p7.c
@@ -0,0 +1,27 @@
+/* Write a program that asks the user to enter a U.S. dollar amount
+ * and then shows how to pay that amount using the smallest number of
+ * $20, $10, $5, and $1 dollar bills
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int dollars, twenties, tens, fives;
+
+	printf("Enter a dollar amount: ");
+	scanf("%d", &dollars);
+
+	twenties = dollars / 20;
+	dollars  = dollars - (twenties * 20);
+	tens     = dollars / 10;
+	dollars  = dollars - (tens * 10);
+	fives    = dollars / 5;
+	/* dollars contains the remaining singles at this point */
+
+	printf("\n$20 bills: %d", twenties);
+	printf("\n$10 bills: %d", tens);
+	printf("\n $5 bills: %d", fives);
+	printf("\n $1 bills: %d", dollars);
+
+	return 0;
+}
