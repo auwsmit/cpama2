19425ee49c630aa037133ae54a10d8498a05cc84
Austin Smith <AssailantLF@gmail.com>
2015-02-02 18:08:57 -0800

    added ch3 project 6

---
+++ Ch3/ProgrammingProjects/p6.c
@@ -0,0 +1,18 @@
+/* Modify the addfrac.c program of Section 3.2 so that the user enters
+ * both fractions at the same time, separated by a plus sign.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, denom1, num2, denom2, result_num, result_denom;
+
+	printf("Enter two fractions separated by a plus sign: ");
+	scanf("%d/%d + %d/%d", &num1, &denom1, &num2, &denom2);
+
+	result_num = num1 * denom2 + num2 * denom1;
+	result_denom = denom1 * denom2;
+	printf("The sum is %d/%d\n", result_num, result_denom);
+
+	return 0;
+}
