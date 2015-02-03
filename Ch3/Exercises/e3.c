d5819d2344ba0613e1d259b4c6cb780865d8b598
Austin Smith <AssailantLF@gmail.com>
2015-02-02 17:07:57 -0800

    added ch3 exercises 3 and 4

---
+++ Ch3/Exercises/e3.c
@@ -0,0 +1,46 @@
+/* For each of the following pairs of scanf format strings,
+ * indicate whether or not the two strings are equivalent.
+ * If they're not, show how they can be distinguished.
+ * (a) "%d"       versus " %d"
+ * (b) "%d-%d-%d" versus "%d -%d -%d"
+ * (c) "%f"       versus "%f "
+ * (d) "%f,%f"    versus "%f, %f"
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float a, b, c;
+	float x, y, z;
+
+	/*scanf("%d", &a);*/
+	/*printf("a1: %d\n", a);*/
+	/*scanf(" %d", &b);*/
+	/*printf("a2: %d\n", b);*/
+
+	/*scanf("%d-%d-%d", &a, &b, &c);*/
+	/*printf("b1: %d, %d, %d\n", a, b, c);*/
+	/*scanf("%d -%d -%d", &x, &y, &z);*/
+	/*printf("b2: %d, %d, %d\n", x, y, z);*/
+
+	/*scanf("%f", &a);*/
+	/*printf("c1: %f\n", a);*/
+	/*scanf("%f ", &b);*/
+	/*printf("c2: %f\n", b);*/
+
+	scanf("%f,%f", &a, &b);
+	printf("d1: %f, %f\n", a, b);
+	scanf("%f, %f", &x, &y);
+	printf("d2: %f, %f\n", x, y);
+
+	return 0;
+}
+
+/* Answer:
+ * (a) No discernible difference.
+ * (b) The first format string doesn't allow blank characters after
+ *     the numbers, but the second does.
+ * (c) The 2nd format string causes scanf to search for an additional
+ *     non-blank character after the number.
+ * (d) No difference.
+ */
---
+++ Ch3/Exercises/e4.c
@@ -0,0 +1,28 @@
+/* Suppose we call scanf as follows: 
+ * scanf("%d%f%d", &i, &x, &j);
+ *
+ * If the user enters
+ * 10.3 5 6
+ *
+ * what will be the values of i, x, and j after the call?
+ * (Assume that i and j are int variables and x is a float variable)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	float x;
+
+	scanf("%d%f%d", &i, &x, &j);
+	printf("i: %d, x: %f, j: %d", i, x, j);
+
+	return 0;
+}
+
+/* Answer: (note: I did figure the answer out on my own first,
+ * but I had to be 100% sure by writing it out and running it)
+ * i = 10
+ * x = 0.300000
+ * j = 5
+ */
