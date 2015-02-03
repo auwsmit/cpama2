8486ff68148527b1bde0cfc11552f7de7d4fd951
Austin Smith <AssailantLF@gmail.com>
2015-02-02 17:14:17 -0800

    added ch3 exercise 5

---
+++ Ch3/Exercises/e5.c
@@ -0,0 +1,27 @@
+/* Suppose we call scanf as follows: 
+ * scanf("%f%d%f", &x, &i, &y);
+ *
+ * If the user enters
+ * 12.3 45.6 789
+ *
+ * what will be the values of x, i, and y after the call?
+ * (Assume that x and y are float variables and i is a int variable)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float x, y;
+	int i;
+
+	scanf("%f%d%f", &x, &i, &y);
+	printf("x: %f, i: %d, y: %f", x, i, y);
+
+	return 0;
+}
+
+/* Answer:
+ * x: 12.300000
+ * i: 45
+ * y: 0.600000
+ */
