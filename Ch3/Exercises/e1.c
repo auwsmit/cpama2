b9d6be73f97c6eb0de7d3a61365a5c802c212dac
Austin Smith <AssailantLF@gmail.com>
2015-02-02 16:47:21 -0800

    added ch3 exercises 1 and 2

---
+++ Ch3/Exercises/e1.c
@@ -0,0 +1,25 @@
+/* What output do the following calls of printf produce?
+ * (a) printf("%6d,%4d\n", 86, 1040);
+ * (b) printf("%12.5e\n", 30.253);
+ * (c) printf("%.4f\n", 83.162);
+ * (d) printf("%-6.2g\n", .0000009979);
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%6d,%4d\n", 86, 1040);
+    printf("%12.5e\n", 30.253);
+    printf("%.4f\n", 83.162);
+    printf("%-6.2g\n", .0000009979);
+
+    return 0;
+}
+
+/* Answer:
+ * Output:
+ * (a) "    86,1040"
+ * (b) "          3.02530e+001"
+ * (c) "83.1620"
+ * (d) "1e-006"
+ */
---
+++ Ch3/Exercises/e2.c
@@ -0,0 +1,28 @@
+/* Write calls of printf that display a float variable x in the 
+ * following formats.
+ * (a) Exponential notation; left-justified in a field of size 8;
+ *     one digit after the decimal point.
+ * (b) Exponential notation; right-justified in a field of size 10;
+ *     six digits after the decimal point.
+ * (c) Fixed decimal notation; left-justified in a field of size 8;
+ *     three digits after the decimal point.
+ * (d) Fixed decimal notation; right-justified in a field of size 6;
+ *     no digits after the decimal point.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float x;
+
+    printf("Please enter a value for x: ");
+    scanf("%f", &x);
+
+    printf("a|%-8.1ea|\n", x);
+    printf("b|%10.6e|\n", x);
+    printf("c|%-8.3d|\n", x);
+    printf("d|%6d|\n", x);
+
+    return 0;
+}
