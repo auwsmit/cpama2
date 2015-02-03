5bba90266d5f07cab81c5ae9d06d9d014a38bb03
Austin Smith <AssailantLF@gmail.com>
2015-02-02 17:47:05 -0800

    added ch3 projects 3 and 4

---
+++ Ch3/ProgrammingProjects/p3.c
@@ -0,0 +1,20 @@
+/* Write a program that breaks down an ISBN entered by the user. */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int prefix, identifier, publisher, itemNum, check;
+
+	printf("Enter ISBN: ");
+	scanf("%d-%d-%d-%d-%d",
+		&prefix, &identifier, &publisher, &itemNum, &check);
+
+	printf("GS1 prefix: %d\n", prefix);
+	printf("Group identifier: %d\n", identifier);
+	printf("Publisher code: %d\n", publisher);
+	printf("Item number: %d\n", itemNum);
+	printf("Check digit: %d\n", check);
+
+	return 0;
+}
---
+++ Ch3/ProgrammingProjects/p4.c
@@ -0,0 +1,17 @@
+/* Write a program that prompts the user to enter a telephone number
+ * in the form (xxx) xxx-xxxx and then displays the number
+ * in the form xxx.xxx.xxx
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n1, n2, n3;
+
+	printf("Enter phone number (xxx) xxx-xxxx: ");
+	scanf("(%d) %d-%d", &n1, &n2, &n3);
+
+	printf("You entered %d.%d.%d", n1, n2, n3);
+
+	return 0;
+}
