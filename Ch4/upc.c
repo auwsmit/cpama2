3b3d0974cdca2995709d6fb04a18c9a15e960c20
Austin Smith <AssailantLF@gmail.com>
2015-02-07 04:50:29 -0800

    fixed a glaring typo (note to self: never assume something works until you run it.)

--- Ch4/upc.c
+++ Ch4/upc.c
@@ -8,11 +8,11 @@ int main(void)
 		first_sum, second_sum, total;
 
 	printf("Enter the first (single) digit: ");
-	scanf("%ld", &d);
+	scanf("%1d", &d);
 	printf("Enter the first group of five digits: ");
-	scanf("%ld%ld%ld%ld%ld", &i1, &i2, &i3, &i4, &i5);
+	scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
 	printf("Enter the second group of five digits: ");
-	scanf("%ld%ld%ld%ld%ld", &j1, &j2, &j3, &j4, &j5);
+	scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
 
 	first_sum = d + i2 + i4 + j1 + j3 + j5;
 	second_sum = i1 + i3 + i5 + j2 + j4;
