c23ba097b16d4ce2aa6c15f43d29ad2ef8b9e9fe
Austin Smith <AssailantLF@gmail.com>
2015-02-17 01:06:51 -0800

    added ch5 examples broker and date

---
+++ Ch5/broker.c
@@ -0,0 +1,31 @@
+/* Calculates a broker's commission */
+
+#include <stdio.h>
+
+int main(void)
+{
+	float commission, value;
+
+	printf("Enter value of trade: ");
+	scanf("%f", &value);
+
+	if (value < 2500.00f)
+		commission = 30.00f + .017f * value;
+	else if (value < 6250.00f)
+		commission = 56.00f + .0066f * value;
+	else if (value < 20000.00f)
+		commission = 76.00f + .0034f * value;
+	else if (value < 50000.00f)
+		commission = 100.00f + .0022f * value;
+	else if (value < 500000.00f)
+		commission = 155.00f + .0011f * value;
+	else
+		commission = 255.0f + .0009f * value;
+
+	if (commission < 39.00f)
+		commission = 39.00f;
+
+	printf("Commission: $%.2f\n", commission);
+
+	return 0;
+}
