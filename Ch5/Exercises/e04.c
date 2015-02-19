b8670046b2b1ee1d02e1c71acfda45e434b512ce
Austin Smith <AssailantLF@gmail.com>
2015-02-18 16:56:48 -0800

    added ch5 exercise 4

---
+++ Ch5/Exercises/e04.c
@@ -0,0 +1,21 @@
+/* Write a single expression whose value is either -1, 0, or +1,
+ * depending on whether i is less than, equal to, or greater than j,
+ * respectively.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	printf("Values for i and j (e.g. 22 6): ");
+	scanf("%d %d", &i, &j);
+
+	printf("Expression: %d", !(i == j) * (i < j ? -1 : 1));
+
+	return 0;
+}
+
+/* Answer:
+ * !(i == j) * (i < j ? -1 : 1)
+ */
