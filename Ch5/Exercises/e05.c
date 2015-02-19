b64f59fcf9ef66abf7d4b7e264c3cb4fed85441e
Austin Smith <AssailantLF@gmail.com>
2015-02-18 17:18:54 -0800

    added ch5 exercises 5 and 6

---
+++ Ch5/Exercises/e05.c
@@ -0,0 +1,28 @@
+/* Is the following if statement legal?
+
+ * if (n >= 1 <= 10)
+ *   printf("n is between 1 and 10\n");
+
+ * If so, what does it do when n is equal to 0?
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+
+	if (n >= 1 <= 10)
+		printf("n is between 1 and 10\n");
+
+	return 0;
+}
+
+/* Answer:
+ * It is a legal statement.
+ *
+ * Here's what happens when n equals 0:
+ *
+ * First, in the if's expression, (n >= 1) is evaluated to 0.
+ * Then, (0 <= 10) is then evaluated as 1, meaning the final value is 1.
+ * Since the value isn't 0, it runs the printf statement.
+ */
---
+++ Ch5/Exercises/e06.c
@@ -0,0 +1,28 @@
+/* Is the following if statement legal?
+
+ * if (n == 1-10)
+ *   printf("n is between 1 and 10\n");
+
+ * If so, what does it do when n is equal to 5?
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 5;
+
+	if (n == 1-10)
+		printf("n is between 1 and 10\n");
+
+	return 0;
+}
+
+/* Answer:
+ * It is a legal statement.
+ *
+ * Here's what happens when n equals 0:
+ *
+ * First, in the if's expression, 1-10 is evaluated to -9.
+ * Then, (n == -9) is then evaluated as 0, meaning the final value is 0.
+ * Since the value is 0, it skips the printf statement and exits.
+ */
