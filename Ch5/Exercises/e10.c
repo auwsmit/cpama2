cfe0785b19786c7d13f1ecc3bc73bbf69073a656
Austin Smith <AssailantLF@gmail.com>
2015-02-19 22:04:15 -0800

    added ch5 exercises 10 and 11

---
+++ Ch5/Exercises/e10.c
@@ -0,0 +1,28 @@
+/* What output does the following program fragment produce? (Assume that i is
+ * an integer variable.)
+ *
+ * i = 1;
+ * switch (i % 3) {
+ *   case 0: printf("zero");
+ *   case 1: printf("one");
+ *   case 2: printf("two");
+ * }
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1;
+
+	switch (i % 3)
+	{
+		case 0: printf("zero");
+		case 1: printf("one");
+		case 2: printf("two");
+			return 0;
+	}
+}
+
+/* Answer:
+ * onetwo
+ */
---
+++ Ch5/Exercises/e11.c
@@ -0,0 +1,55 @@
+/* The following table shows telephone area codes in the state of Georgia with
+ * the largest city in each area:
+ *
+ * Area code  Major City
+ *    229     Albany
+ *    404     Atlanta
+ *    470     Atlanta
+ *    478     Macon
+ *    678     Atlanta
+ *    706     Columbus
+ *    762     Columbus
+ *    770     Atlanta
+ *    912     Savannah
+ *
+ * Write a switch statement whose controlling expression is the variable
+ * area_code. If the value of area_code is in the table, the switch statement
+ * will print the corresponding city name. Otherwise, the switch statement will
+ * display the message "Area code not recognized". Use techniques discussed in
+ * Section 5.3 to make the switch statement as simple as possible.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int area_code;
+
+	printf("Enter area code (3 numbers): ");
+	scanf("%d", &area_code);
+
+	switch (area_code)
+	{
+		case 229:
+			printf("Albany");
+			break;
+		case 404: case 470: case 678: case 770:
+			printf("Atlanta");
+			break;
+		case 478:
+			printf("Macon");
+			break;
+		case 706: case 762:
+			printf("Columbus");
+			break;
+		case 912:
+			printf("Savannah");
+			break;
+		default:
+			printf("Area code not recognized");
+	}
+	return 0;
+}
+
+/* Answer:
+ *
+ */
