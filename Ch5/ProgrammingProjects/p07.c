561ef7b4dc927b607daa95bc1a3372af80bd7641
Austin Smith <AssailantLF@gmail.com>
2015-02-22 13:37:47 -0800

    added ch5 project 7

---
+++ Ch5/ProgrammingProjects/p07.c
@@ -0,0 +1,38 @@
+/* Write a program that finds the largest and smallest of four integers entered
+ * by the user:
+ *
+ * Enter four integers: 21 43 10 35
+ * Largest: 43
+ * Smallest 10
+ *
+ * Use as few if statements as possible. Hint: Four if statements are sufficient.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i1, i2, i3, i4, largest, smallest;
+
+	printf("Enter four integers: ");
+	scanf("%d %d %d %d", &i1, &i2, &i3, &i4);
+
+	largest = smallest = i1;
+
+	if (largest < i2)
+		largest = i2;
+	if (largest < i3)
+		largest = i3;
+	if (largest < i4)
+		largest = i4;
+
+	if (smallest > i2)
+		smallest = i2;
+	if (smallest > i3)
+		smallest = i3;
+	if (smallest > i4)
+		smallest = i4;
+
+	printf("Largest: %d\nSmallest: %d", largest, smallest);
+
+	return 0;
+}
