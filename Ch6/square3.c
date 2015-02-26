cd23334d5fb3fa860c070b119ac34108e049a6f1
Austin Smith <AssailantLF@gmail.com>
2015-02-26 13:21:36 -0800

    added a few ch6 examples

---
+++ Ch6/square3.c
@@ -0,0 +1,23 @@
+/* Prints a table of squares using an odd method */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, n, odd, square;
+
+	printf("This program prints a table of squares.\n");
+	printf("Enter the number of entries in a table: ");
+	scanf("%d", &n);
+
+	i = 1;
+	odd = 3;
+	for (square = 1; i <= n; odd += 2)
+	{
+		printf("%10d%10d\n", i, i * i);
+		++i;
+		square += odd;
+	}
+
+	return 0;
+}
