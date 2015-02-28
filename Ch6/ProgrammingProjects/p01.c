ee352e0ee70a9ae36dd3c391744d8b084fb8e5e7
Austin Smith <AssailantLF@gmail.com>
2015-02-27 17:41:53 -0800

    added ch6 project 1

---
+++ Ch6/ProgrammingProjects/p01.c
@@ -0,0 +1,35 @@
+/* Write a program that finds the largest in a series of numbers entered by the
+ * user. The program must prompt the user to enter numbers one by one. When the
+ * user enters 0 or a negative number, the program must display the largest
+ * nonnegative number entered:
+ *
+ * Enter a number: 60
+ * Enter a number: 38.3
+ * Enter a number: 4.89
+ * Enter a number: 100.62
+ * Enter a number: 75.2295
+ * Enter a number: 0
+ *
+ * The largest number entered was 100.62
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float i, n, largest;
+
+	i = 1;
+	do
+	{
+		printf("Enter a number: ");
+		scanf("%f", &n);
+
+		if (n == 1 || n > largest) largest = n;
+
+		i++;
+	} while (i <= 6);
+
+	printf("\nThe largest number entered was %.2f", largest);
+
+	return 0;
+}
