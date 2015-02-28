e89c8c614fd95614d6289e873c9fa408bc7aa832
Austin Smith <AssailantLF@gmail.com>
2015-02-28 00:25:39 -0800

    added ch6 projects 2 & 3

---
+++ Ch6/ProgrammingProjects/p02.c
@@ -0,0 +1,32 @@
+/* Write a program that asks the user to enter two integers, then calculates
+ * and displays their greatest common divisor (DCD):
+ *
+ * Enter two integers: 12 28
+ * Greatest common divisor: 4
+ *
+ * Hint: The classic algorithm for computing the GCD, known as Euclid's
+ * algorithm, goes as follows: Let m and n be variables containing the two
+ * numbers. If n is 0, then stop: m contains the GCD. Otherwise, compute the
+ * remainder when m is divided by n. Copy n into m and copy the remainder into
+ * n. Then repeat the procfess, starting with testing whether n is 0.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int m, n, remainder;
+
+	printf("Enter two integers: ");
+	scanf("%d %d", &m, &n);
+
+	do
+	{
+		remainder = m % n;
+		m = n;
+		n = remainder;
+	} while (n != 0);
+
+	printf("Greatest common divisor: %d", m);
+
+	return 0;
+}
---
+++ Ch6/ProgrammingProjects/p03.c
@@ -0,0 +1,36 @@
+/* Write a program that asks the user to enter a fraction, then reduceds the
+ * fraction to lowest terms:
+ *
+ * Enter a fraction: 6/12
+ * In lowest terms: 1/2
+ *
+ * Hint: To reduce a fraction to lowest terms, first compute the GCD of the
+ * numerator and denominator. Then divide both the numerator and demoninator by
+ * the GCD.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int m, n, remainder, m_reduced, n_reduced;
+
+	printf("Enter a fraction: ");
+	scanf("%d/%d", &m, &n);
+
+	m_reduced = m;
+	n_reduced = n;
+
+	do
+	{
+		remainder = m % n;
+		m = n;
+		n = remainder;
+	} while (n != 0);
+
+	m_reduced /= m;
+	n_reduced /= m;
+
+	printf("In lowest terms: %d/%d", m_reduced, n_reduced);
+
+	return 0;
+}
