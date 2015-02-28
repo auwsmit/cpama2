3d90fdcb5666ed29c99678fc2299bd901630e75c
Austin Smith <AssailantLF@gmail.com>
2015-02-28 12:39:07 -0800

    added ch6 projects 7 & 8

---
+++ Ch6/ProgrammingProjects/p07.c
@@ -0,0 +1,23 @@
+/* Rearrange the square3.c program so that the for loop initializes i,
+ * tests i, and increments i. Don't rewrite the program; in particular,
+ * don't use any multiplications
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i, n, odd, square;
+
+	printf("This program prints a table of squares.\n");
+	printf("Enter the number of entries in a table: ");
+	scanf("%d", &n);
+
+	odd = 3;
+	for (square = 1, i = 1; i <= n; odd += 2, i++)
+	{
+		printf("%10d%10d\n", i, i * i);
+		square += odd;
+	}
+
+	return 0;
+}
---
+++ Ch6/ProgrammingProjects/p08.c
@@ -0,0 +1,50 @@
+/* Write a program that prints a one-month calendar. The user specifies
+ * the number of days in the month and the day of the week on which the
+ * month begins:
+ *
+ * Enter number of days in month: 31
+ * Enter starting day of the week (1=Sun, 7=Sat): 3
+ *
+ *        1  2  3  4  5
+ *  6  7  8  9 10 11 12
+ * 13 14 15 16 17 18 19
+ * 20 21 22 23 24 25 26
+ * 27 28 29 30 31
+ *
+ * Hint: This program isn't as hard as it looks. The most important part
+ * is a for statement that uses a variable i to count from 1 to n, where
+ * n is the number of days in the month, printing each value of i.
+ * Inside the loop, an if statement tests whether i is the last day of
+ * the week; if so, it prints the new-line character.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int month_days, starting_day, i, p;
+
+	printf("Enter number of days in month: ");
+	scanf("%d", &month_days);
+	printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+	scanf("%d", &starting_day);
+	printf("\n");
+
+	for (p = 1, i = 1; p <= month_days; p++, i++)
+	{
+
+		if (i < starting_day)
+		{
+			printf("   ");
+			p--;
+		}
+		else if (i % 7 == 0)
+		{
+			printf("%2d ", p);
+			printf("\n");
+		}
+		else
+			printf("%2d ", p);
+	}
+
+	return 0;
+}
