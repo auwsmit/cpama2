69404d00aef08f89ad53d1d2d3d9eb32b44476fc
Austin Smith <AssailantLF@gmail.com>
2015-03-01 15:12:59 -0800

    added ch7 example sum2.c

---
+++ Ch7/sum2.c
@@ -0,0 +1,21 @@
+/* Sums a series of numbers (using long variables) */
+
+#include <stdio.h>
+
+int main(void)
+{
+	long n, sum = 0;
+
+	printf("This program sums a series of integers.\n");
+	printf("Enter integers (0 to terminate): ");
+
+	scanf("%ld", &n);
+	while (n != 0)
+	{
+		sum += n;
+		scanf("%ld", &n);
+	}
+	printf("The sum is: %ld\n", sum);
+
+	return 0;
+}
