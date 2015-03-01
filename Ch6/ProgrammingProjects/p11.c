60a296f32c6712c4e2cd8ececeec542434066c91
Austin Smith <AssailantLF@gmail.com>
2015-03-01 10:42:23 -0800

    added ch6 projects 11 & 12 (incomplete)

---
+++ Ch6/ProgrammingProjects/p11.c
@@ -0,0 +1,10 @@
+/* The value of the mathematical constant e can be expressed as an
+ * infinite series:
+ *
+ * e = 1 + 1/1! + 1/2! + 1/3! + ...
+ * 
+ * Write a program that approximates e by computing the value of
+ * 1 + 1/1! + 1/2! + 1/3! + ... + 1/n!
+ * where n is an integer entered by the user.
+ */
+/* Not 100% sure how to do this, so incomplete */
---
+++ Ch6/ProgrammingProjects/p12.c
@@ -0,0 +1,4 @@
+/* Modify Programming Project 11 so that the program continues adding
+ * terms until the current term becomes less than e, where e is a small
+ * (floating-point) number entered by the user. */
+/* Incomplete until project 11 is done */
