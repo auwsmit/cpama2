d04b30981e668de4f267f7e7c05808cc06634d3e
Austin Smith <AssailantLF@gmail.com>
2015-03-03 17:30:18 -0800

    added ch7 exercise 1

---
+++ Ch7/Exercises/e01.c
@@ -0,0 +1,21 @@
+/* Give the value of each of the following integer constants.
+ * (a) 077
+ * (b) 0x77
+ * (c) 0XABC
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("A: %d\n", 077);
+	printf("B: %d\n", 0x77);
+	printf("C: %d\n", 0XABC);
+	printf("\n");
+	return 0;
+}
+
+/* Answer:
+ * A - 63
+ * B - 119
+ * C - 2748
+ */
