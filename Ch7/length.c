4e25ca9882530ec5d4f88247f0dec088d4ef8890
Austin Smith <AssailantLF@gmail.com>
2015-03-03 17:30:00 -0800

    added ch7 examples length.c & length2.c

---
+++ Ch7/length.c
@@ -0,0 +1,20 @@
+/* Determines the length of a message */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+	int len = 0;
+
+	printf("Enter a message: ");
+	ch = getchar();
+	while (ch != '\n')
+	{
+		len++;
+		ch = getchar();
+	}
+	printf("Your message was %d characters long.\n", len);
+
+	return 0;
+}
---
+++ Ch7/length2.c
@@ -0,0 +1,15 @@
+/* Determines the length of a message */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int len = 0;
+
+	printf("Enter a message: ");
+	while (getchar() != '\n')
+		len++;
+	printf("Your message was %d characters long.\n", len);
+
+	return 0;
+}
