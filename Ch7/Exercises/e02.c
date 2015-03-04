dd4930866fa66e4473cdc1356678430109bc967b
Austin Smith <AssailantLF@gmail.com>
2015-03-04 14:02:26 -0800

    added ch7 exercises 2, 3, & 4

---
+++ Ch7/Exercises/e02.c
@@ -0,0 +1,24 @@
+/* Which of the following are not legal constants in C? Classify each
+ * legal constant as either integer or floating-point.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("A: %f\n", 010E2);
+	printf("B: %f\n", 32.1E+5);
+	/* printf("C: %d\n", 0790); */
+	/* printf("D: %f\n", 100_000); */
+	printf("E: %f\n", 3.978e-2);
+
+	return 0;
+}
+
+/* Answer:
+ * A: legal float
+ * B: legal float
+ * C: illegal, octal doesn't use 9
+ * D: illegal, no underscores in constants
+ * E: legal float
+ */
---
+++ Ch7/Exercises/e03.c
@@ -0,0 +1,24 @@
+/* Which of the following are not legal types in C?
+ * (a) short unsigned int
+ * (b) short float
+ * (c) long double
+ * (d) unsigned long
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	short unsigned int a;
+	/* short float b; */
+	long double c;
+	unsigned long d;
+
+	return 0;
+}
+
+/* Answer:
+ * A - legal
+ * B - illegal
+ * C - legal
+ * D - legal
+ */
