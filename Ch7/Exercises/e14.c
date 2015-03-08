85a9be45d96f9c3880ac6f521c68f9933b47e7ea
Austin Smith <AssailantLF@gmail.com>
2015-03-08 16:00:39 -0700

    added ch7 exercises 11, 12, 13, & 14

---
+++ Ch7/Exercises/e14.c
@@ -0,0 +1,26 @@
+/* Does the following statement always compute the fractional part of
+ * f correctly (assuming that f and frac_part are float variables)?
+ *
+ * fract_part = f - (int) f;
+ *
+ * If not, what's the problem?
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float f, fract_part;
+
+	printf("Enter a fractional number (e.g. 1.55): ");
+	scanf("%f", &f);
+
+	fract_part = f - (int) f;
+	printf("Fractional part: %f\n", fract_part);
+
+	return 0;
+}
+
+/* Answer:
+ * No, it will fail if the value of f exceeds the largest
+ * value of an int.
+ */
