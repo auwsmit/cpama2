85186174ebffa0b2cbeeb24899378fb0bec2960e
Austin Smith <AssailantLF@gmail.com>
2015-03-10 14:15:47 -0700

    added ch7 projects 4 & 5

---
+++ Ch7/ProgrammingProjects/p04.c
@@ -0,0 +1,62 @@
+/* Write a program that translates an alphabetic phone number into numeric
+ * form:
+ *
+ * Enter phone number: CALLATT
+ * 225288
+ *
+ * (In case you don't have a telephone nearby, here are the letters on the
+ * 2=ABC, 3=DEF, 4=GHI, 5=JKL, 6=MNO, 7=PRS, 8=TUV, 9=WXY.) If the original
+ * phone number contains nonalphanumeric characters (digits or punctuation,
+ * for example), leave them unchanged:
+ *
+ * Enter phone number: 1-800-COL-LECT
+ * 1-800-265-5328
+ *
+ * You may assume that any letters entered by the user are upper case.
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char ch;
+
+	printf("Enter phone number: ");
+
+	while ((ch = getchar()) != '\n')
+	{
+		switch (toupper(ch))
+		{
+			case 'A': case 'B': case 'C':
+				printf("2");
+				break;
+			case 'D': case 'E': case 'F':
+				printf("3");
+				break;
+			case 'G': case 'H': case 'I':
+				printf("4");
+				break;
+			case 'J': case 'K': case 'L':
+				printf("5");
+				break;
+			case 'M': case 'N': case 'O':
+				printf("6");
+				break;
+			case 'P': case 'R': case 'S':
+				printf("7");
+				break;
+			case 'T': case 'U': case 'V':
+				printf("8");
+				break;
+			case 'W': case 'X': case 'Y':
+				printf("9");
+				break;
+			default:
+				putchar(ch);
+		}
+	}
+
+	printf("\n");
+
+	return 0;
+}
---
+++ Ch7/ProgrammingProjects/p05.c
@@ -0,0 +1,68 @@
+/* In the SCRABBLE Crossord Game, players form words using small tiles,
+ * each containing a letter and a face value. The face value varies from
+ * one letter to another, based on the letter's rarity. (Here are the
+ * face values: 1: AEILNORSTU, 2: DG, 3: BCMP, 4: FHVWY, 5: K, 8: JX,
+ * 10: QZ.) Write a program that computes the value of a word by summing
+ * the values of its letters:
+ *
+ * Enter a word: pitfall
+ * Scrabble value: 12
+ *
+ * Your program should allow any mixture of lower-case and upper-case
+ * letters in the word. Hint: Use the toupper library function.
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char ch;
+	int score = 0;
+
+	printf("Enter a word: ");
+
+	while ((ch = getchar()) != '\n')
+	{
+		switch (toupper(ch))
+		{
+			case 'A': case 'E':
+			case 'I': case 'L':
+			case 'N': case 'O':
+			case 'R': case 'S':
+			case 'T': case 'U':
+				score += 1;
+				break;
+
+			case 'D': case 'G':
+				score += 2;
+				break;
+
+			case 'B': case 'C':
+			case 'M': case 'P':
+				score += 3;
+				break;
+
+			case 'F':
+			case 'H': case 'V':
+			case 'W': case 'Y':
+				score += 4;
+				break;
+
+			case 'K':
+				score += 5;
+				break;
+
+			case 'J': case 'X':
+				score += 8;
+				break;
+
+			case 'Q': case 'Z':
+				score += 10;
+				break;
+		}
+	}
+
+	printf("Scrabble value: %d\n", score);
+
+	return 0;
+}
