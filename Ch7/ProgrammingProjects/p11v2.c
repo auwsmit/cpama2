bff7e6309769b1a05987441f52e99309c34059e0
Austin Smith <AssailantLF@gmail.com>
2015-03-11 00:31:24 -0700

    added ch7 project 11 (revised) & 12

---
+++ Ch7/ProgrammingProjects/p11v2.c
@@ -0,0 +1,32 @@
+/* Write a program that takes the first name and last name entered by
+ * the user and displays the last name, a comma, and the first
+ * initial, followed by a period:
+ *
+ * Enter a first and last name: Lloyd Fosdick
+ * Fosdick, L.
+ *
+ * Ths user's input may contain extra spaces before the first name,
+ * between the first and last names, and after the last name.
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char first_name_letter, input;
+
+	printf("Enter a first and last name: ");
+	scanf(" %c", &first_name_letter);
+
+	while (getchar() != ' ');
+
+	while ((input = getchar()) != '\n')
+	{
+		if (input != ' ')
+			putchar(input);
+	}
+
+	printf(", %c.\n", first_name_letter);
+
+	return 0;
+}
---
+++ Ch7/ProgrammingProjects/p12.c
@@ -0,0 +1,64 @@
+/* Write a program that evaluates an expression:
+ *
+ * Enter an expression: 1+2.5*3
+ * Value of expression: 10.5
+ *
+ * The operands in the expression are floating-point numbers; the operators
+ * are +, -, *, and /. The expression is evaluated from left to right (no
+ * operator takes precedence over any other operator).
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	float valueA, valueB, valueC;
+	char operator1, operator2;
+
+	valueA = valueB = valueC = 0;
+
+	printf("Enter an expression: ");
+	scanf(" %f %c %f %c %f",
+		&valueA, &operator1, &valueB, &operator2, &valueC);
+
+	switch(operator1)
+	{
+		case '+':
+			valueA += valueB;
+			break;
+		case '-':
+			valueA -= valueB;
+			break;
+		case '*':
+			valueA *= valueB;
+			break;
+		case '/':
+			valueA /= valueB;
+			break;
+		default:
+			printf("Invalid operator\n");
+			return 0;
+	}
+
+	switch(operator2)
+	{
+		case '+':
+			valueA += valueC;
+			break;
+		case '-':
+			valueA -= valueC;
+			break;
+		case '*':
+			valueA *= valueC;
+			break;
+		case '/':
+			valueA /= valueC;
+			break;
+		default:
+			printf("Invalid operator\n");
+			return 0;
+	}
+
+	printf("Value of an expression: %.1f", valueA);
+
+	return 0;
+}
