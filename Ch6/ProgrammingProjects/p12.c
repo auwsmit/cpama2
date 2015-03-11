f1b37ae8989714d145fd63946c8ca98f98b0b486
Austin Smith <AssailantLF@gmail.com>
2015-03-10 20:03:19 -0700

    added ch6 project 12

--- Ch6/ProgrammingProjects/p12.c
+++ Ch6/ProgrammingProjects/p12.c
@@ -1,4 +1,38 @@
 /* Modify Programming Project 11 so that the program continues adding
  * terms until the current term becomes less than e, where e is a small
  * (floating-point) number entered by the user. */
-/* Incomplete until project 11 is done */
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	float precision;
+	float end_result, factorial, p;
+
+	printf("Enter a number: ");
+	scanf("%f", &precision);
+
+	end_result = 1;
+
+	printf("\ne = 1");
+	for (i = 1;; i++)
+	{
+		/* calculating the factorial */
+		factorial = p = i;
+		while (p > 0)
+		{
+			if (p - 1 == 0) break;
+			factorial *= --p;
+		}
+
+		end_result += (1 / factorial);
+		printf(" + 1/%d!", i);
+
+		if ((1 / factorial) < precision)
+			break;
+	}
+
+	printf(" = %.4f\n", end_result);
+
+	return 0;
+}
