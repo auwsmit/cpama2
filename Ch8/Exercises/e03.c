3b57dd7069afa79912aa217d9c872562e5a70ef4
Austin Smith <AssailantLF@gmail.com>
2015-03-13 21:48:13 -0700

    added ch8 exercise 4

--- Ch8/Exercises/e03.c
+++ Ch8/Exercises/e03.c
@@ -1,11 +1,13 @@
-/* Write a declaration of an array named weekend containing seven bool values. Include an initializer that makes the first and last values true; all other values should be false.
+/* Write a declaration of an array named weekend containing seven bool values.
+ * Include an initializer that makes the first and last values true; all other
+ * values should be false.
  */
 #include <stdbool.h> /* C99 only */
 #include <stdio.h>
 
 int main(void)
 {
-    bool weekend[] = { [0] = true, [6] = true };
+    bool weekend[] = { true, false, false, false, false, false, true };
     int i;
 
     for (i = 0; i < 7; i++)
---
+++ Ch8/Exercises/e04.c
@@ -0,0 +1,17 @@
+/* (C99) Repeat Exercise 3, but this time use a designated initializer.  Make
+ * the initializer as short as possible.
+ */
+#include <stdbool.h> /* C99 only */
+#include <stdio.h>
+
+int main(void)
+{
+    bool weekend[] = { [0] = true, [6] = true };
+    int i;
+
+    for (i = 0; i < 7; i++)
+        printf("\nDay %d: Weekend %d", i + 1, weekend[i]);
+    printf("\n");
+
+    return 0;
+}
