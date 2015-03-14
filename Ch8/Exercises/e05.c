e4d4032f562926eaffd265dc225e38e8c9245960
Austin Smith <AssailantLF@gmail.com>
2015-03-14 01:15:40 -0700

    added ch8 exercise 5

---
+++ Ch8/Exercises/e05.c
@@ -0,0 +1,11 @@
+/* The Fibonacci numbers are 0, 1, 1, 2, 3, 5, 8, 13, ..., where each number is
+ * the sum of the two preceding numbers. Write a program fragment that declares
+ * an array named fib_numbers of length 40 and fills the array with the first
+ * 40 Fibonacci numbers, Hint: Fill in the first two numbers individually, then
+ * use a loop to compute the remaining numbers.
+ */
+
+int fib_numbers[40] = { 0, 1 }, i;
+
+for (i = 2; i < 40; i++)
+    fib_numbers[i] = fib_numbers[i-2] + fib_numbers[i-1];
