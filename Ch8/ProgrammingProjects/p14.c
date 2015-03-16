62d770d3c3fbb17b051df2f74f9e2f855a1a2382
Austin Smith <AssailantLF@gmail.com>
2015-03-16 10:24:26 -0700

    added ch8 project 14

---
+++ Ch8/ProgrammingProjects/p14.c
@@ -0,0 +1,62 @@
+/* Write a program that reverses words in a sentence:
+ *
+ * Enter a sentence: you can cage a swallow can't you?
+ * Reversal of sentence: you can't swallow a cage can you?
+ *
+ * Hint: Use a loop to read the characters one by one and store them in
+ * a one-dimensional char array. Have the loop stop at a period, question mark,
+ * or exclamation point (the "terminating character"), which is saved in
+ * a separate char variable. Then use a second loop to search backward through
+ * the array for the beginning of the last word. Print the last word, then
+ * search backward for the next-to-last word. Repeat until the beginning of the
+ * array is  reached. Finally, print the terminating character.
+ */
+#include <stdio.h>
+
+#define BUFFER 50
+
+int main(void)
+{
+    char input, terminator = 0, sentence[BUFFER] = {0};
+    int i, j, last_space;
+
+    last_space = BUFFER;
+
+    printf("\nEnter a sentence: ");
+    for (i = 0; (input = getchar()) != '\n'; i++)
+    {
+        if (input == '.' || input == '?' || input == '!')
+        {
+            terminator = input;
+            break;
+        }
+        sentence[i] = input;
+    }
+
+    if (terminator == 0)
+    {
+        printf("Sentence needs a terminating character. (./?/!)\n\n");
+        return 0;
+    }
+
+    printf("Reversal of sentence: ");
+    for (i = BUFFER - 1; i >= 0; i--)
+    {
+        if (sentence[i] == ' ')
+        {
+            for (j = i + 1; j != last_space; j++)
+            {
+                putchar(sentence[j]);
+            }
+            last_space = i;
+            putchar(sentence[i]);
+        }
+    }
+    for (; i != last_space; i++)
+    {
+        putchar(sentence[i]);
+    }
+    printf("%c\n\n", terminator);
+
+    return 0;
+}
