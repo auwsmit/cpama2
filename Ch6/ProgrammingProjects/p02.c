94c729f1ac51513a7fa57429c644db378a4ed73a
Austin Smith <AssailantLF@gmail.com>
2015-03-17 22:33:44 -0700

    fixed a typo

--- Ch6/ProgrammingProjects/p02.c
+++ Ch6/ProgrammingProjects/p02.c
@@ -8,7 +8,7 @@
  * algorithm, goes as follows: Let m and n be variables containing the two
  * numbers. If n is 0, then stop: m contains the GCD. Otherwise, compute the
  * remainder when m is divided by n. Copy n into m and copy the remainder into
- * n. Then repeat the procfess, starting with testing whether n is 0.
+ * n. Then repeat the process, starting with testing whether n is 0.
  */
 #include <stdio.h>
 
