dbdc4e345941fe8ecbadaee4e713daf2ae247415
Austin Smith <AssailantLF@gmail.com>
2015-03-26 15:39:10 -0700

    added ch11 example minmax.c

---
+++ Ch11/maxmin.c
@@ -0,0 +1,37 @@
+/* Finds the largest and smallest elements in an array */
+
+#include <stdio.h>
+
+#define N 10
+
+void max_min(int a[], int n, int *max, int *min);
+
+int main(void)
+{
+    int b[N], i, big, small;
+
+    printf("Enter %d numbers: ", N);
+    for (i = 0; i < N; i++)
+        scanf("%d", &b[i]);
+
+    max_min(b, N, &big, &small);
+
+    printf("Largest: %d\n", big);
+    printf("Smallest: %d\n", small);
+
+    return 0;
+}
+
+void max_min(int a[], int n, int *max, int *min)
+{
+    int i;
+
+    *max = *min = a[0];
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] > *max)
+            *max = a[i];
+        else if (a[i] < *min)
+            *min = a[i];
+    }
+}
