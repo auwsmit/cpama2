a7b6588ee4d72e0a09beb05143cc9a0e2f56a7ff
Austin Smith <AssailantLF@gmail.com>
2015-04-01 12:41:37 -0700

    fixed small typos

--- Ch05/ProgrammingProjects/p08.c
+++ Ch05/ProgrammingProjects/p08.c
@@ -28,13 +28,15 @@
 int main(void)
 {
     int input_h, input_m, t8_00am, t9_43am, t11_19am,
-        t12_47pm, t2_00pm, t3_45pm, t7_00pm, t9_45pm, tmidnight;
+        t12_47pm, t2_00pm, t3_45pm, t7_00pm, t9_45pm,
+        tmidnight24, tmidnight0;
 
     printf("\nEnter a 24-hour time: ");
     scanf("%d:%d", &input_h, &input_m);
 
     input_m = input_h * 60 + input_m;
 
+    /* departure times as minutes since midnight */
     t8_00am = (60 * 8);
     t9_43am = (60 * 9) + 43;
     t11_19am = (60 * 11) + 19;
@@ -43,16 +45,19 @@ int main(void)
     t3_45pm = (60 * 15) + 45;
     t7_00pm = (60 * 19);
     t9_45pm = (60 * 21) + 45;
-    tmidnight = (60 * 24);
 
-    if (input_m >= 60 * 24 || input_m < 0)
+    /* both representations of midnight */
+    tmidnight24 = (60 * 24);
+    tmidnight0 = 0;
+
+    if (input_m >= tmidnight24 || input_m < tmidnight0)
     {
         printf("Entered invalid time.");
     }
     else
     {
         printf("Closest departure time is ");
-        if (input_m >= 0 && input_m < t8_00am)
+        if (input_m >= tmidnight0 && input_m < t8_00am)
             printf("8:00 a.m., arriving at 10:16 a.m.");
 
         if (input_m >= t8_00am && input_m < t9_43am)
@@ -67,19 +72,19 @@ int main(void)
             if (input_m - t9_43am < t11_19am - input_m)
                 printf("9:43 a.m., arriving at 11:52 a.m.");
             else
-                printf("11:43 a.m., arriving at 1:31 p.m.");
+                printf("11:19 a.m., arriving at 1:31 p.m.");
         }
         if (input_m >= t11_19am && input_m < t12_47pm)
         {
             if (input_m - t11_19am < t12_47pm - input_m)
-                printf("11:43 a.m., arriving at 1:31 p.m.");
+                printf("11:19 a.m., arriving at 1:31 p.m.");
             else
-                printf("12:47 p.m., arriving at 2:00 p.m.");
+                printf("12:47 p.m., arriving at 3:00 p.m.");
         }
         if (input_m >= t12_47pm && input_m < t2_00pm)
         {
             if (input_m - t12_47pm < t2_00pm - input_m)
-                printf("12:47 p.m., arriving at 2:00 p.m.");
+                printf("12:47 p.m., arriving at 3:00 p.m.");
             else
                 printf("2:00 p.m., arriving at 4:08 p.m.");
         }
@@ -105,7 +110,7 @@ int main(void)
                 printf("9:45 p.m., arriving at 11:58 p.m.");
         }
 
-        if (input_m >= t9_45pm && input_m < tmidnight)
+        if (input_m >= t9_45pm && input_m < tmidnight24)
             printf("9:45 p.m., arriving at 11:58 p.m.");
     }
 
