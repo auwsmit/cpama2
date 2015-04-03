88d3f69ee755f90ba59b501b3f62a5ecd0eb7caa
Austin Smith <AssailantLF@gmail.com>
2015-04-03 00:41:59 -0700

    updated ch12 exercise 8

--- Ch12/Exercises/e08.c
+++ Ch12/Exercises/e08.c
@@ -1,4 +1,6 @@
-/* Rewrite the following function to use pointer arithmetic instead of array subscripting. (In other words, eliminate the variable i and all uses of the [] operator.) Make as few changes as possible.
+/* Rewrite the following function to use pointer arithmetic instead of array
+ * subscripting. (In other words, eliminate the variable i and all uses of the
+ * [] operator.) Make as few changes as possible.
  *
  * void store_zeros(int a[], int n)
  * {
