e08b63c9ba031d364a6653dc415152328472a33e
Austin Smith <AssailantLF@gmail.com>
2015-04-30 12:30:00 -0700

    added ch15 projects 1 and 2

---
+++ Ch15/ProgrammingProjects/p1/line.c
@@ -0,0 +1,77 @@
+/* The justify program of Section 15.3 justifies the lines by inserting extra
+ * spaces beteen words. The way the write_line function currently works, the
+ * words closer to the end of a line tend to have slightly wider gaps between
+ * them than the words at the beginning. (For example, the words closer to the
+ * end might have three spaces between them, while the words closer to the
+ * beginning might be separated by only two spaces.) Improve the program by
+ * having write_line alternate between putting larger gaps at the end of the
+ * line and putting them at the beginning of the line.
+ */
+#include <stdio.h>
+#include <string.h>
+#include "line.h"
+
+#define true 1
+#define false 0
+typedef int bool;
+
+#define MORE_SPACES_LEFT true
+#define MORE_SPACES_RIGHT false
+#define MAX_LINE_LEN 60
+
+char line[MAX_LINE_LEN+1];
+int line_len = 0;
+int num_words = 0;
+bool space_pref = false;
+
+void clear_line(void)
+{
+    line[0] = '\0';
+    line_len = 0;
+    num_words = 0;
+}
+
+void add_word(const char *word)
+{
+    if (num_words > 0) {
+        line[line_len] = ' ';
+        line[line_len+1] = '\0';
+        line_len++;
+    }
+    strcat(line, word);
+    line_len += strlen(word);
+    num_words++;
+}
+
+int space_remaining(void)
+{
+    return MAX_LINE_LEN - line_len;
+}
+
+void write_line(void)
+{
+    int extra_spaces, spaces_to_insert, i, j;
+
+    space_pref = !space_pref;
+    extra_spaces = MAX_LINE_LEN - line_len;
+    for (i = 0; i < line_len; i++) {
+        if (line[i] != ' ')
+            putchar(line[i]);
+        else {
+            spaces_to_insert = extra_spaces / (num_words - 1);
+            if (space_pref == MORE_SPACES_LEFT && extra_spaces > 0)
+                spaces_to_insert++;
+            for (j = 1; j <= spaces_to_insert + 1; j++)
+                putchar(' ');
+            extra_spaces -= spaces_to_insert;
+            num_words--;
+        }
+    }
+    putchar('\n');
+}
+
+void flush_line(void)
+{
+    if (line_len > 0)
+        puts(line);
+}
---
+++ Ch15/ProgrammingProjects/p2/word.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "word.h"
+
+int read_char(void)
+{
+    int ch = getchar();
+
+    if (ch == '\n' || ch == '\t')
+        return ' ';
+    return ch;
+}
+
+void read_word(char *word, int len)
+{
+    int ch, pos = 0;
+
+    while((ch = read_char()) == ' ')
+        ;
+    while(ch != ' ' && ch != EOF) {
+        if (pos < len)
+            word[pos++] = ch;
+        ch = read_char();
+    }
+    if ((pos - 1) == (len - 1))
+        word[len-1] = '*';
+    word[pos] = '\0';
+}
