6ede53009758b195a941815e83e15d785d79a6ba
Austin Smith <AssailantLF@gmail.com>
2015-05-01 12:21:21 -0700

    added ch15 project 5

---
+++ Ch15/ProgrammingProjects/p5/stack.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+#define STACK_SIZE 100
+
+/* external variables */
+char contents[STACK_SIZE];
+int top = 0;
+
+bool is_empty(void)
+{
+    return top == 0;
+}
+
+bool is_full(void)
+{
+    return top == STACK_SIZE;
+}
+
+void push(char i)
+{
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = i;
+}
+
+char pop(void)
+{
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+}
+
+void stack_overflow()
+{
+    printf("Expression is too complex\n\n");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow()
+{
+    printf("Not enough operands in expression\n\n");
+    exit(EXIT_FAILURE);
+}
