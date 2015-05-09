43edc7d3f24c3c9fa9fabcf1b3e2c8561b4d40b1
Austin Smith <AssailantLF@gmail.com>
2015-05-09 15:32:46 -0700

    added ch16 project 4 & example inventory makefile

---
+++ Ch16/ProgrammingProjects/p4/read_line.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "read_line.h"
+
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+
+    while (isspace(ch = getchar()))
+        ;
+    while (ch != '\n' && ch != EOF) {
+        if (i < n)
+            str[i++] = ch;
+        ch = getchar();
+    }
+    str[i] = '\0';
+    return i;
+}
